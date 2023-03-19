e10d980330a2f40d71b2940ce98690edfd9e5940
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-19 17:13:12 +0700

    Update ly_4.py

--- ly_4.py
+++ ly_4.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('KẾT QUẢ HỌC TẬP LỚP LÝ 4')
-y = st.text_input('Nhập họ và tên')
+y = st.text_input('Nhập họ và tên (Lưu ý: ghi hoa chữ cái đầu)')
 if st.button('Kết quả'):
     df = pd.read_excel('DS_10Ly4.xlsx')
     df1 = df[df['Họ và tên'] == y]
