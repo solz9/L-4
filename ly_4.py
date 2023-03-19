bcdb006db99dc15210c376fb6065a2afc102bcf1
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-19 13:54:49 +0700

    Update ly_4.py

--- ly_4.py
+++ ly_4.py
@@ -11,9 +11,8 @@ if st.button('Kết quả'):
     BTDDS = df[df['Họ và tên'] == y]['BT01 Đúng/Sai']
     BTMM = df[df['Họ và tên'] == y]['BT02 Moment']
     Bonus = df[df['Họ và tên'] == y]['Điểm cộng']
-    a = np.array([Names, HS1, BTDDS, BTMM, Bonus]).unique()
-    st.write({'Họ và tên': a[0], 'HS1': a[1], 'BT01 Đúng/Sai': a[2], 'BT02 Moment':a[3], 'Điểm cộng': a[4]})
-
+    a = np.array([Names, HS1, BTDDS, BTMM, Bonus])
+    st.write({'Họ và tên': Names, 'HS1': HS1, 'BT01 Đúng/Sai': BTDDS, 'BT02 Moment':BTMM, 'Điểm cộng': Bonus})
 # "name": ["An", "Bình", "Châu", "Nam", "Mai"], 
 #     "grade": [7, 6, 5, 7, 9], 
 #     "class": ["10A1", "10A2", "10A3", "10B", "10C"],
