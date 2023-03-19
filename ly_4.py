730b28c526ad50293815787480c401b86ba2d4a6
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-19 13:16:53 +0700

    Update ly_4.py

--- ly_4.py
+++ ly_4.py
@@ -1,10 +1,9 @@
-
+import pickle
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-y = st.text_input('Nhập họ và tên')
 st.title('KẾT QUẢ HỌC TẬP LỚP LÝ 4')
+y = st.text_input('Nhập họ và tên')
 if st.button('Kết quả'):
     df = pickle.load(open('ds_ly4', 'rb'))
     r = df[df['Họ và tên'] == y]['Họ và tên'], df[df['Họ và tên'] == y]['Điểm cộng'],df[df['Họ và tên'] == y]['BT01 Đúng/Sai'],df[df['Họ và tên'] == y]['BT02 Moment']
