537f4d1ae1291b3750f9ca58fb82f49857f1ac97
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-19 13:52:40 +0700

    Update ly_4.py

--- ly_4.py
+++ ly_4.py
@@ -6,13 +6,13 @@ st.title('KẾT QUẢ HỌC TẬP LỚP LÝ 4')
 y = st.text_input('Nhập họ và tên')
 if st.button('Kết quả'):
     df = pickle.load(open('ds_ly4', 'rb'))
-    Names = df[df['Họ và tên'] == y]['Họ và tên'].values
+    Names = df[df['Họ và tên'] == y]['Họ và tên']
     HS1 = df[df['Họ và tên'] == y]['HS1']
     BTDDS = df[df['Họ và tên'] == y]['BT01 Đúng/Sai']
     BTMM = df[df['Họ và tên'] == y]['BT02 Moment']
     Bonus = df[df['Họ và tên'] == y]['Điểm cộng']
-    a = np.array([Names, HS1, BTDDS, BTMM, Bonus]) 
-    st.write({'Họ và tên': Names, 'HS1': HS1, 'BT01 Đúng/Sai': BTDDS, 'BT02 Moment':BTMM, 'Điểm cộng': Bonus})
+    a = np.array([Names, HS1, BTDDS, BTMM, Bonus]).values
+    st.write({'Họ và tên': a[0], 'HS1': a[1], 'BT01 Đúng/Sai': a[2], 'BT02 Moment':a[3], 'Điểm cộng': a[4]})
 
 # "name": ["An", "Bình", "Châu", "Nam", "Mai"], 
 #     "grade": [7, 6, 5, 7, 9], 
