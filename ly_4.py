c301acd92a5e5e29b7550e4184723a91d7e0bd1b
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-19 13:01:31 +0700

    Update ly_4.py

--- ly_4.py
+++ ly_4.py
@@ -1,10 +1,12 @@
+pip install streamlit-aggrid
 import streamlit as st
+from st_aggrid import AgGrid
 import pandas as pd
 import numpy as np
 df = pd.read_excel('DS_10Ly4.xlsx')
 df = pd.DataFrame(df)
 df = df.drop(columns=['Họ và tên đệm', 'Tên'])
-
+AgGrid(df)
 st.title('KẾT QUẢ HỌC TẬP LỚP LÝ 4')
 a = st.number_input('Nhập họ và tên')
 y = input('Họ và tên')
