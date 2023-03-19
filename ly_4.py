38223a760d3a7742197fb6bbca607ddf1f6776c8
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-19 13:13:38 +0700

    Update ly_4.py

--- ly_4.py
+++ ly_4.py
@@ -1,16 +1,12 @@
 
 import streamlit as st
-install streamlit-aggrid
-from st_aggrid import AgGrid
 import pandas as pd
 import numpy as np
-df = pd.read_excel('DS_10Ly4.xlsx')
-df = pd.DataFrame(df)
-df = df.drop(columns=['Họ và tên đệm', 'Tên'])
-AgGrid(df)
+
+y = st.number_input('Nhập họ và tên')
 st.title('KẾT QUẢ HỌC TẬP LỚP LÝ 4')
-a = st.number_input('Nhập họ và tên')
-y = input('Họ và tên')
 if st.button('Kết quả'):
-  st.write(df[df['Họ và tên'] == y]['Họ và tên'], df[df['Họ và tên'] == y]['Điểm cộng'],df[df['Họ và tên'] == y]['BT01 Đúng/Sai'],df[df['Họ và tên'] == y]['BT02 Moment'])
+    df = pickle.load(open('ds_ly4', 'rb'))
+    r = df[df['Họ và tên'] == y]['Họ và tên'], df[df['Họ và tên'] == y]['Điểm cộng'],df[df['Họ và tên'] == y]['BT01 Đúng/Sai'],df[df['Họ và tên'] == y]['BT02 Moment']
+    st.write(r)
 
